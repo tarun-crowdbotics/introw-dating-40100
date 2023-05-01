943f3271ad54b214c1aa90b1dad885e8354eba9e
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-05-01 15:58:36 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "shy-dust-40100",
-  url: "https://shy-dust-40100.botics.co",
-  api: "https://shy-dust-40100.botics.co/api/v1"
+  "name": "shy-dust-40100",
+  "url": "https://shy-dust-40100.botics.co",
+  "api": "https://shy-dust-40100.botics.co/api/v1",
+  "initialRoute": "onboarding"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
