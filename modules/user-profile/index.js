7c150777eee2e02849c2e8699e0e5940ce4b2e6d
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-05-01 16:45:34 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/user-profile/index.js
+++ modules/user-profile/index.js
@@ -6,10 +6,11 @@ import { styles, Color } from "./styles";
 import { unwrapResult } from "@reduxjs/toolkit";
 import { EditUser } from "./edit";
 import ViewUser from "./view";
+export const UserProfile = ({
+  route
+}) => {
+  const [isEdit, setIsEdit] = useState(false); // code below depends on the existence of any login module - update as needed.
 
-export const UserProfile = ({ route }) => {
-  const [isEdit, setIsEdit] = useState(false);
-  // code below depends on the existence of any login module - update as needed.
   const login = useSelector(state => {
     return state?.login;
   });
@@ -17,41 +18,25 @@ export const UserProfile = ({ route }) => {
   const user = useSelector(state => state.userProfile.users[userId]);
   const api = useSelector(state => state.userProfile.api);
   const dispatch = useDispatch();
-
   useEffect(async () => {
     if (userId) {
-      dispatch(getUserById(userId))
-        .then(unwrapResult)
-        .then(response => {
-          const edit = response.id === login?.user.id;
-          setIsEdit(edit);
-        })
-        .catch(e => console.log(e));
+      dispatch(getUserById(userId)).then(unwrapResult).then(response => {
+        const edit = response.id === login?.user.id;
+        setIsEdit(edit);
+      }).catch(e => console.log(e));
     }
   }, [userId]);
-
-  return (
-    <ScrollView style={styles.container} contentStyle={styles.content}>
-      {api.loading === "pending"
-        ? (
-        <View>
+  return <ScrollView style={styles.container} contentStyle={styles.content}>
+      {api.loading === "pending" ? <View>
           <ActivityIndicator color={Color.steel} />
-        </View>
-          )
-        : (
-        <View>
+        </View> : <View>
           <View>{!user && <Text>No user to display information.</Text>}</View>
-          {user && (
-            <View>
+          {user && <View>
               {isEdit ? <EditUser user={user} /> : <ViewUser user={user} />}
-            </View>
-          )}
-        </View>
-          )}
-    </ScrollView>
-  );
+            </View>}
+        </View>}
+    </ScrollView>;
 };
-
 export default {
   title: "userProfile",
   navigator: UserProfile,
