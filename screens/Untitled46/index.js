122953e98c15a456111c0e1e6118e1d66959aea7
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-13 07:44:37 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled46/index.js
+++ screens/Untitled46/index.js
@@ -1,20 +1,63 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
 const Untitled46 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}>
+        <TouchableOpacity style={styles.button}>
+          <Text>Back</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.headerText}>Connection History</Text>
+
+        <View style={styles.detailsView}>
+          <Text>List of all my connections</Text>
+        </View>
+
+        <TouchableOpacity style={styles.clickable}>
+          <Text>Select Specific Connection</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.clickable}>
+          <Text>See more</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text>Close</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  button: {
+    backgroundColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 5
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 10
+  },
+  detailsView: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 5
+  },
+  clickable: {
+    padding: 10,
+    marginVertical: 5
   }
 });
 export default Untitled46;
