bb15c5e61904b636924695faa3f9f320d5aa541b
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-13 07:57:23 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled42/index.js
+++ screens/Untitled42/index.js
@@ -1,20 +1,52 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, Text, TouchableOpacity } from "react-native";
 
 const Untitled42 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}>
+        <TouchableOpacity style={styles.button}>
+          <Text>Back</Text>
+        </TouchableOpacity>
+        <Text style={styles.title}>Counterpart Social Ally</Text>
+        <TouchableOpacity style={styles.selectAlly}>
+          <Text>Select Specific Ally</Text>
+        </TouchableOpacity>
+        <Text>Social Ally Name: John Doe</Text>
+        <Text>Social Ally Email: john.doe@example.com</Text>
+        <Text>Social Ally Location: New York, USA</Text>
+        <Text>Social Ally Phone Number: +1 (123) 456-7890</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text>Chat</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  button: {
+    backgroundColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  selectAlly: {
+    backgroundColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20
   }
 });
 export default Untitled42;
