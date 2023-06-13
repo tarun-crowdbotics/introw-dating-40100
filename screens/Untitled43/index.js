95ac311169b5aea425f09c9b1ba245907303b5c8
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-13 07:49:28 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled43/index.js
+++ screens/Untitled43/index.js
@@ -1,20 +1,71 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, Text, TouchableOpacity } from "react-native";
 
-const Untitled43 = () => {
+const ResourceLibrary = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}>
+        <Text style={styles.headerText}>
+          Resource library with videos about love, finding love, Love tips
+        </Text>
+        <TouchableOpacity style={styles.button}>
+          <Text>Back</Text>
+        </TouchableOpacity>
+        <Text style={styles.detailedView}>Resource Library</Text>
+        <Text style={styles.subHeader}>Videos Materials</Text>
+        <TouchableOpacity>
+          <Text style={styles.clickable}>Love</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.clickable}>Finding Love</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.clickable}>Love Tips</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.clickable}>Select Specific Video</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.clickable}>Play Video</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10
+  },
+  detailedView: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  subHeader: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  clickable: {
+    fontSize: 12,
+    color: "blue",
+    textDecorationLine: "underline",
+    marginBottom: 10
   }
 });
-export default Untitled43;
+export default ResourceLibrary;
