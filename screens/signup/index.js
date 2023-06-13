698a92a37c7dfc255975f56d913a73fbbf50e196
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-13 08:22:11 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/signup/index.js
+++ screens/signup/index.js
@@ -1,193 +1,99 @@
-import React from "react";
-import { Text, View, TouchableOpacity, Image, StyleSheet, TextInput, TouchableHighlight } from "react-native";
-
-const pressed = () => {
-  console.log("pressed");
-};
+import React, { useState } from "react";
+import { Text, View, TouchableOpacity, StyleSheet, TextInput, CheckBox } from "react-native";
 
 const Signup = () => {
-  return <View style={styles.container}>
-      <View style={styles.heading}>
-        <Text style={styles.headingText}>Sign up</Text>
-      </View>
-      <View>
-        <View style={styles.emailContainer}>
-          <Text style={styles.mr10}>Email address</Text>
-          <Input placeholder='Email' />
-        </View>
-        <View style={styles.mb20}>
-          <Text style={styles.mr10}>Password</Text>
-          <Input placeholder='Enter' />
-        </View>
-        <View style={styles.mb20}>
-          <Text style={styles.mr10}>Confirm password</Text>
-          <Input placeholder='Enter' />
-        </View>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
 
-        <View style={styles.loginContainer}>
-          <Button onPress={pressed}>Sign up</Button>
-        </View>
-        <View style={styles.orContainer}>
-          <View style={styles.line} />
-          <Text style={styles.orText}>Or</Text>
-          <View style={styles.line} />
-        </View>
-        <View style={styles.imageContainer}>
-          <View style={styles.iconContainer}>
-            <Image source={require("./assets/appleIcon.png")} style={styles.icon} />
-          </View>
-          <View style={styles.iconContainer}>
-            <Image source={require("./assets/googleIcon.png")} style={styles.icon} />
-          </View>
-          <View style={styles.iconContainer}>
-            <Image source={require("./assets/fbIcon.png")} style={styles.icon} />
-          </View>
-        </View>
+  const handleSignUp = () => {
+    console.log("Sign up");
+  };
+
+  const handleTermsAndPrivacy = () => {
+    console.log("Terms and Privacy");
+  };
+
+  return <View style={styles.container}>
+      <Text style={styles.heading}>Sign up</Text>
+      <TextInput style={styles.input} placeholder="Username" value={username} onChangeText={setUsername} />
+      <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <View style={styles.checkboxContainer}>
+        <CheckBox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+        <Text style={styles.label}>
+          I agree to the{" "}
+          <Text onPress={handleTermsAndPrivacy} style={styles.link}>
+            Terms and Conditions
+          </Text>{" "}
+          and{" "}
+          <Text onPress={handleTermsAndPrivacy} style={styles.link}>
+            Privacy Policy
+          </Text>
+        </Text>
       </View>
-      <View style={styles.footerContainer}>
-        <Text style={styles.footerText}>I have an account? </Text>
-        <TouchableOpacity onPress={pressed}>
-          <Text>Login</Text>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Sign up</Text>
+      </TouchableOpacity>
+      <Text style={styles.orText}>Or</Text>
+      <View style={styles.socialContainer}>
+        {
+        /* Add social icons here */
+      }
       </View>
     </View>;
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    height: "100%",
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
     backgroundColor: "#fff"
   },
   heading: {
-    alignSelf: "center"
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center"
   },
-  headingText: {
-    fontSize: 42,
-    fontWeight: "500"
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5
   },
-  emailContainer: {
-    marginBottom: 10
-  },
-  mr10: {
-    marginRight: 10,
-    marginBottom: 10
-  },
-  mb20: {
+  checkboxContainer: {
+    flexDirection: "row",
     marginBottom: 20
   },
-  forgotPassword: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 40
-  },
-  loginContainer: {
-    width: "80%",
+  checkbox: {
     alignSelf: "center"
   },
-  orContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "70%",
-    alignSelf: "center"
+  label: {
+    margin: 8
   },
-  line: {
-    height: 1,
-    width: 100,
-    backgroundColor: "rgba(0, 0, 0, 0.05)"
+  link: {
+    color: "blue"
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center"
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold"
   },
   orText: {
-    marginVertical: 40,
-    alignSelf: "center",
-    fontSize: 16,
-    color: "#231F20",
-    opacity: 0.5
+    textAlign: "center",
+    marginVertical: 10
   },
-  imageContainer: {
-    display: "flex",
+  socialContainer: {
     flexDirection: "row",
-    alignItems: "center",
-    width: "60%",
-    alignSelf: "center",
-    justifyContent: "space-between"
-  },
-  iconContainer: {
-    height: 40,
-    width: 40,
-    padding: 10,
-    borderRadius: 10,
-    backgroundColor: "#F7F7F7"
-  },
-  icon: {
-    height: 18,
-    width: 18
-  },
-  footerContainer: {
-    alignSelf: "center",
-    display: "flex",
-    flexDirection: "row"
-  },
-  footerText: {
-    color: "#6B6B6B"
+    justifyContent: "space-around"
   }
 });
 export default Signup;
-
-const Button = props => {
-  return <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
-      <View style={[btnStyles.button, {
-      backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
-      height: props.height ? props.height : 49,
-      borderWidth: props.borderWidth ? props.borderWidth : 0,
-      borderColor: props.borderColor ? props.borderColor : "#000000"
-    }]}>
-        <Text style={[btnStyles.text, {
-        color: props.color ? props.color : "#ffffff"
-      }]}>{props.children}</Text>
-      </View>
-    </TouchableHighlight>;
-};
-
-const btnStyles = StyleSheet.create({
-  button: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10
-  },
-  text: {
-    fontWeight: "bold",
-    fontSize: 15
-  }
-});
-
-const Input = props => {
-  return <View>
-      <TextInput style={textStyles.input} placeholder={props.placeholder} value={props.value} onChangeText={num => props.setValue(num)} placeholderTextColor='#ddd' editable={props.editable !== false} />
-      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
-    </View>;
-};
-
-const textStyles = StyleSheet.create({
-  input: {
-    backgroundColor: "#fff",
-    height: 53,
-    borderColor: "#C4C4C4",
-    color: "#000",
-    borderRadius: 10,
-    fontSize: 14,
-    borderWidth: 1,
-    paddingHorizontal: 10
-  },
-  error: {
-    fontSize: 13,
-    color: "#FA060D",
-    paddingTop: 8
-  }
-});
