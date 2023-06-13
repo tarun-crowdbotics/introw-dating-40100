6a7fdffe3a4a780649f655b0fe5a53d3df23a3ef
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-13 07:15:26 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI36/index.js
+++ screens/ScreenAI36/index.js
@@ -1,88 +1,98 @@
-import { StyleSheet } from "react-native";
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, ScrollView, Button, DatePickerAndroid, Picker } from 'react-native';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 
-const ProfileScreen = () => {
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [fullName, setFullName] = useState('John Doe');
-  const [username, setUsername] = useState('johndoe');
-  const [email, setEmail] = useState('john.doe@example.com');
-  const [bio, setBio] = useState('Lorem ipsum dolor sit amet.');
-  const [dob, setDob] = useState('');
-  const [gender, setGender] = useState('');
-  const [address, setAddress] = useState({
-    street: '',
-    city: '',
-    zip: '',
-    state: '',
-    country: ''
-  });
-
-  const openDatePicker = async () => {
-    try {
-      const {
-        action,
-        year,
-        month,
-        day
-      } = await DatePickerAndroid.open({
-        date: new Date()
-      });
-
-      if (action !== DatePickerAndroid.dismissedAction) {
-        setDob(`${month + 1}/${day}/${year}`);
-      }
-    } catch ({
-      code,
-      message
-    }) {
-      console.warn('Cannot open date picker', message);
-    }
-  };
-
-  return <ScrollView>
-      <View>
-        <Button title="Request To Be Attached as a Social Ally" onPress={() => {}} />
-        <Image source={{
+const App = () => {
+  return <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.icon} source={{
         uri: 'https://tinyurl.com/42evm3m3'
-      }} style={_styles.pKOfeiCA} />
-        {isEditMode ? <TextInput value={fullName} onChangeText={setFullName} placeholder="Full Name" /> : <Text>{fullName}</Text>}
-        {isEditMode ? <TextInput value={username} onChangeText={setUsername} placeholder="Username" /> : <Text>{username}</Text>}
-        {isEditMode ? <TextInput value={email} onChangeText={setEmail} placeholder="Email" keyboardType="email-address" /> : <Text>{email}</Text>}
-        {isEditMode ? <TextInput value={bio} onChangeText={setBio} placeholder="Bio" multiline /> : <Text>{bio}</Text>}
-        {isEditMode ? <TouchableOpacity onPress={openDatePicker}>
-            <Text>{dob || 'Select Date of Birth'}</Text>
-          </TouchableOpacity> : <Text>{dob}</Text>}
-        {isEditMode ? <Picker selectedValue={gender} onValueChange={itemValue => setGender(itemValue)}>
-            <Picker.Item label="Female" value="female" />
-            <Picker.Item label="Male" value="male" />
-            <Picker.Item label="Prefer not to say" value="other" />
-          </Picker> : <Text>{gender}</Text>}
-        {isEditMode ? <TextInput value={address.street} onChangeText={text => setAddress(prev => ({ ...prev,
-        street: text
-      }))} placeholder="Street Address" /> : <Text>{address.street}</Text>}
-        {isEditMode ? <TextInput value={address.city} onChangeText={text => setAddress(prev => ({ ...prev,
-        city: text
-      }))} placeholder="City" /> : <Text>{address.city}</Text>}
-        {isEditMode ? <TextInput value={address.zip} onChangeText={text => setAddress(prev => ({ ...prev,
-        zip: text
-      }))} placeholder="Zip Code" /> : <Text>{address.zip}</Text>}
-        {isEditMode ? <TextInput value={address.state} onChangeText={text => setAddress(prev => ({ ...prev,
-        state: text
-      }))} placeholder="State" /> : <Text>{address.state}</Text>}
-        {isEditMode ? <TextInput value={address.country} onChangeText={text => setAddress(prev => ({ ...prev,
-        country: text
-      }))} placeholder="Country" /> : <Text>{address.country}</Text>}
-        <Button title={isEditMode ? 'Save' : 'Edit'} onPress={() => setIsEditMode(!isEditMode)} />
+      }} />
+        <Image style={styles.icon} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
+        <Image style={styles.icon} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
+        <Image style={styles.icon} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
       </View>
-    </ScrollView>;
+      <ScrollView>
+        <View style={styles.mainMenu}>
+          <Image style={styles.icon} source={{
+          uri: 'https://tinyurl.com/42evm3m3'
+        }} />
+          <TouchableOpacity style={styles.button}>
+            <Text>Message</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>My Match</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>Report/flag Users</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>Connection History</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.button}>
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>Settings</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>My Social Allies</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>Wisdom Tree</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>Intro Cosmic</Text>
+        </TouchableOpacity>
+      </View>
+    </View>;
 };
 
-export default ProfileScreen;
-
-const _styles = StyleSheet.create({
-  pKOfeiCA: {
-    width: 100,
-    height: 100
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+  icon: {
+    width: 30,
+    height: 30
+  },
+  mainMenu: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 5,
+    width: '80%',
+    alignItems: 'center'
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    padding: 10
   }
 });
+export default App;
