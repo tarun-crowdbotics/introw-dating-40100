dd4240b9ade1a9ccd91e3cf8bb2c6df5fec21443
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-13 07:26:36 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled38/index.js
+++ screens/Untitled38/index.js
@@ -1,20 +1,51 @@
 import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, Text, Button, View, Image } from "react-native";
 
 const Untitled38 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}>
+        <Button title="Back" onPress={() => {}} />
+        <Text style={styles.headerText}>My Match</Text>
+        <Text style={styles.preFilledText}>Full Name</Text>
+        <Text style={styles.preFilledText}>Date Email</Text>
+        <Image style={styles.profilePic} source={{
+        uri: "https://example.com/profile-pic.jpg"
+      }} />
+        <Text style={styles.preFilledText}>Location</Text>
+        <Text style={styles.preFilledText}>Phone Number</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="Chat" onPress={() => {}} />
+        </View>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10
+  },
+  preFilledText: {
+    fontSize: 18,
+    marginBottom: 5
+  },
+  profilePic: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10
+  },
+  buttonContainer: {
+    marginTop: 10
   }
 });
 export default Untitled38;
