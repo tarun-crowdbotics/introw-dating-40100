250ffe2f4bea30fe8f703780fe7e6ba9d94a6cbe
Crowdbotics <engineering@crowdbotics.com>
2023-07-02 17:52:05 +0000

    Update store.

--- store/shydustAPI/api.js
+++ store/shydustAPI/api.js
@@ -1,84 +1,59 @@
-import axios from "axios";
+import axios from "axios"
 const shydustAPI = axios.create({
   baseURL: "https://introw-dating-40100.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return shydustAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return shydustAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return shydustAPI.post(`/api/v1/login/`, payload.data);
+  return shydustAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return shydustAPI.post(`/api/v1/signup/`, payload.data);
+  return shydustAPI.post(`/api/v1/signup/`, payload.data)
 }
-
 function modules_camera_photos_user_list(payload) {
-  return shydustAPI.get(`/modules/camera/photos/user/`);
+  return shydustAPI.get(`/modules/camera/photos/user/`)
 }
-
 function modules_camera_photos_user_retrieve(payload) {
-  return shydustAPI.get(`/modules/camera/photos/user/${payload.id}/`);
+  return shydustAPI.get(`/modules/camera/photos/user/${payload.id}/`)
 }
-
 function modules_camera_upload_image_create(payload) {
-  return shydustAPI.post(`/modules/camera/upload_image/`);
+  return shydustAPI.post(`/modules/camera/upload_image/`)
 }
-
 function rest_auth_login_create(payload) {
-  return shydustAPI.post(`/rest-auth/login/`, payload.data);
+  return shydustAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return shydustAPI.get(`/rest-auth/logout/`);
+  return shydustAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return shydustAPI.post(`/rest-auth/logout/`);
+  return shydustAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return shydustAPI.post(`/rest-auth/password/change/`, payload.data);
+  return shydustAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return shydustAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return shydustAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return shydustAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return shydustAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
 }
-
 function rest_auth_registration_create(payload) {
-  return shydustAPI.post(`/rest-auth/registration/`, payload.data);
+  return shydustAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return shydustAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return shydustAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return shydustAPI.get(`/rest-auth/user/`);
+  return shydustAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return shydustAPI.put(`/rest-auth/user/`, payload.data);
+  return shydustAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return shydustAPI.patch(`/rest-auth/user/`, payload.data);
+  return shydustAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -97,4 +72,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
--- store/shydustAPI/logins.slice.js
+++ store/shydustAPI/logins.slice.js
@@ -1,16 +1,13 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const rest_auth_login_create = createAsyncThunk("logins/rest_auth_login_create", async payload => {
-  const response = await apiService.rest_auth_login_create(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const rest_auth_login_create = createAsyncThunk(
+  "logins/rest_auth_login_create",
+  async payload => {
+    const response = await apiService.rest_auth_login_create(payload)
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const loginsSlice = createSlice({
   name: "logins",
   initialState,
@@ -18,24 +15,21 @@ const loginsSlice = createSlice({
   extraReducers: {
     [rest_auth_login_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
     [rest_auth_login_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload);
-        state.api.loading = "idle";
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
       }
     },
     [rest_auth_login_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
-export default {
-  rest_auth_login_create,
-  slice: loginsSlice
-};
+})
+export default { rest_auth_login_create, slice: loginsSlice }
